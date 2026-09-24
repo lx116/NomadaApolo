ab6e40965834467c89d4566af458e8b7cdf85c02
luisvelez <luisenriquevelezsantos@gmail.com>
2026-09-24 09:15:48 -0500

    audio.py

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import AudioUploadView
+
+urlpatterns = [
+    path('upload/', AudioUploadView.as_view()),
+]
+
---
+++ api/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from api.serializer import AudioSerializer
+
+
+# Create your views here.
+class AudioUploadView(APIView):
+    authentication_classes = []
+    permission_classes = []
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request, *args, **kwargs):
+        serializer = AudioSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
